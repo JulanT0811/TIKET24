ff9a6b5968383c396d989f214945562d384001da
ELMartin31611 <martinborja31611@gmail.com>
2024-12-02 11:24:47 -0500

    Update calculos.py

--- calculos.py
+++ calculos.py
@@ -1,1 +1,14 @@
+def calcularCuota(monto,interes,meses):
+    interesmensual=interes/12
+    inter=interesmensual/100
+    cuotamensual=(monto*inter)/(1-(1+inter)**(-meses))
+    return cuotamensual
 
+import datetime
+def calcularEdad (fecha):
+    anio_actual = datetime.datetime.now().year
+    if fecha >= 0 and fecha <= anio_actual:
+        edad = anio_actual - fecha
+        return edad
+    else:
+        return -1
